a952d48f6ed13b53c945c9f754ea91ee188b1875
Joyce Lau <jshizuki@yahoo.com.hk>
2023-06-27 22:12:47 +0900

    Slightly refactored

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,14 +11,15 @@ class GamesController < ApplicationController
     url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
     data = JSON.parse(URI.open(url).read)
     match_or_not = params[:word].chars.all? { |letter| params[:letters].include?(letter) }
+    @word = params[:word].upcase
     @result = if data['found'] == true && match_or_not
-                "Congratulations! #{params[:word].upcase} is a valid English word!"
+                "Congratulations! #{@word} is a valid English word!"
               elsif data['found'] == true && match_or_not == false
-                "Sorry but #{params[:word].upcase} can't be built out of #{params[:letters].upcase}"
+                "Sorry but #{@word} can't be built out of #{params[:letters].upcase}"
               elsif data['found'] == false && match_or_not
-                "Sorry but #{params[:word].upcase} doesn't seem to be a valid English word..."
+                "Sorry but #{@word} doesn't seem to be a valid English word..."
               else
-                "Sorry but #{params[:word].upcase} doesn't seem to be a valid English word or match any of the letters..."
+                "Sorry but #{@word} doesn't seem to be a valid English word or match any of the letters..."
               end
   end
 end
