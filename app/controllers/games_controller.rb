17251a4bcca8712d31159ef4db75070445003d31
Joyce Lau <jshizuki@yahoo.com.hk>
2023-06-27 21:40:36 +0900

    Display result on score page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,5 @@
+# require 'open-uri'
+
 class GamesController < ApplicationController
   def new
     @vowels = %w[a e i o u].sample(3)
@@ -6,6 +8,17 @@ class GamesController < ApplicationController
   end
 
   def score
-    raise
+    url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
+    data = JSON.parse(URI.open(url).read)
+    match_or_not = params[:word].chars.all? { |letter| params[:letters].include?(letter) }
+    @result = if data['found'] == true && match_or_not
+                "Congratulations! #{params[:word].upcase} is a valid English word!"
+              elsif data['found'] == true && match_or_not == false
+                "Sorry but #{params[:word].upcase} can't be built out of #{params[:letters].upcase}"
+              elsif data['found'] == false && match_or_not
+                "Sorry but #{params[:word].upcase} doesn't seem to be a valid English word..."
+              else
+                "Sorry but #{params[:word].upcase} doesn't seem to be a valid English word or match any of the letters..."
+              end
   end
 end
